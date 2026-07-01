0fda0f4171743fd89ecbeba7319e0b12be117694
Олександр Кубарич <kuseas5@gmail.com>
2026-07-02 00:13:46 +0300

    Initialize solution with `.gitignore`, basic project structure, and `TwoSumSolution` skeleton.

---
+++ LeetCode/TwoSumSolution.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode;
+
+/// <summary>
+/// url:
+/// </summary>
+public class TwoSumSolution
+{
+    /// <summary>
+    /// <param name="nums">Only one valid answer exists.</param>>
+    /// </summary>
+    public int[] TwoSum(int[] nums, int target)
+    {
+        throw new NotImplementedException();
+    }
+}
