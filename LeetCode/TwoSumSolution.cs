cc5c3acd10cd560f51a168a0df421747b62d8085
Олександр Кубарич <kuseas5@gmail.com>
2026-07-02 00:14:59 +0300

    Implement `TwoSum` method with helper function to find indices of target-sum pairs.

--- LeetCode/TwoSumSolution.cs
+++ LeetCode/TwoSumSolution.cs
@@ -10,6 +10,27 @@ public class TwoSumSolution
     /// </summary>
     public int[] TwoSum(int[] nums, int target)
     {
+        var second = 0;
+        
+        for (var i = second; i < nums.Length; i++)
+        {
+            var first = FindIndexLessTargetItem(nums, i, target);
+            second = FindIndexLessTargetItem(nums, first + 1, target);
+            if (nums[first] + nums[second] == target) return [first, second];
+        }
+        throw new NotImplementedException();
+    }
+
+    private int FindIndexLessTargetItem(int[] nums, int index, int target)
+    {
+        for (var i = index; i < nums.Length; i++)
+        {
+            var subject = nums[i];
+            if (subject > target)
+                continue;
+
+            return i;
+        }
         throw new NotImplementedException();
     }
 }
